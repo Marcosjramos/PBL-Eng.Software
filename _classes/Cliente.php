c5ba0e19e59903e54417a9a2a29fba9de0d4e233
Marcos ramos <mjramosfsa@gmail.com>
2016-09-30 11:18:03 -0300

    Galera   coloquei  as  classes Cliente , Prestador, PrestadorPro e Usuario na pasta classes , e  commitei  as  Telas quem-somos, termos-uso(precimaos  ver   o quei  colocar só   estrutureia tela )e fale conosco.

--- _classes/Cliente.php
+++ _classes/Cliente.php
@@ -1,89 +1,33 @@
 <?php
 
-   class Cliente implements Usuario{
+class Cliente extends Usuario {
 
-   	 private $Nome;
-   	 private $Sexo;
-   	 private $Endereco;
-   	 private $CPF;
-   	 private $Tel;
-     private $Email;
-     private $Senha;
+	private $telefone;
+	private $endereco;
+
+	public function __construct($nome, $cpf, $email, $senha, $sexo, $telefone, $endereco)
+	{
+		parent::__construct($nome, $cpf, $email, $senha, $sexo);
+		$this->telefone = $telefone;
+		$this->endereco = $endereco;
+	}
 
 
-      public function __construct($nome,$sexo,$endereco,$cpf,$tel,$email,$senha){
+	public function getTelefone() {
+		return $this->telefone;
+	}
+
+	public function getEndereco() {
+		return $this->endereco;
+	}
 	
-	  	$this->Nome=$nome;
-	  	$this->Sexo=$sexo;
-	  	$this->Endereco=$endereco;
-	  	$this->CPF=$cpf;
-	  	$this->Tel=$tel;
-      $this->Email=$email;
-      $this->Senha=$senha;
-		
-	    }
-       public function getEmail(){
-          return $this->Email;
-        }
+	public function setTelefone($telefone) {
+		$this->telefone = $telefone;
+	}
 
-        public function setEmail($email){
-          $this->Email=$email;
-        }
-        
-        public function setSenha($senha){
-          $this->Senha=$senha;
-        }
+	public function setEndereco($endereco) {
+		$this->endereco = $endereco;
+	}
+}
 
-        public  function setCPF($cpf){
-          $this->CPF=$cpf;
-
-        }
-
-        public function getCPF(){
-        	return $this->CPF;
-        }
-
-        public  function setNome($nome){
-        	 $this->Nome=$nome;
-
-        }
-
-        public  function getNome(){
-        	return $this->Nome;
-
-        }
-
-        public  function setSexo($sexo){
-        	$this->Sexo=$sexo;
-
-        }
-
-        public  function getSexo(){
-        	return $this->Sexo;
-
-        }
-
-        public  function setEndereco($endereco){
-            $this->Endereco=$endereco;
-
-        }
-
-        public function getEndereco(){
-        	return $this->endereco;
-
-        }
-
-        public function setTel($tel){
-        	$this->Tel=$tel;
-
-        }
-
-        public function getTel(){
-        	return $this->Tel;
-
-        }
-        
-   	
-   }
-
-  ?>
+?>
