3c414c988322f83407eb2928838eef1686ea6d37
Felipe Damasceno <felipedamasceno97@gmail.com>
2016-10-06 22:38:12 -0300

    apagando tudo

--- README.md
+++
@@ -1,2 +0,0 @@
-# PBL-Eng.Software
-Projeto do PBL de Engenharia de Software
--- index.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Este arquivo não faz nada, apenas inclui os arquivos necessários
- */
- 
-// Config
-require_once 'config.php';
