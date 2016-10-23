2d2e21781af3ec539b902a8084026fc48de9a5f6
gilvaneibispo <gilvanei.pereira@gmail.com>
2016-10-23 16:37:57 -0300

    Versão 0.1[Cadastro Funcionando].

--- controller/PrestadorController.class.php
+++
@@ -1,6 +0,0 @@
-<?php
-$_SESSION['title'] = TITLE.' : Home';
-
-require_once PATH . "/view/includes/cabecalho.php";
-require_once PATH . "/view/perfil-prestador-view.php";
-require_once PATH . "/view/includes/footer.php";
