2a9abb169706a410009eb10ba3602b70a8d593d7
Lenington Rios <lenitorios@gmail.com>
2016-10-24 21:55:26 -0200

    Serviço view

---
+++ view/buscar-servico-view.php
@@ -0,0 +1,56 @@
+<div class="animate-box">
+    <div class="container">
+        <div class="post">
+            <div class="col-md-8 col-md-offset-2 col-md-push-2 animate-box">
+                <div style="height: 2em"></div>
+                <h2 >Fale Conosco</h2>
+                <p>Tem dúvidas sobre como utilizar o serviço, sugestões ou reclamação? Deixe uma mensagem abaixo que nossa equipe de suporte entrará em contato o mais rápido possível.</p><br><br>
+
+            </div>
+            <div class="col-md-3">
+                <h3>Infornações de Contato</h3>
+                <ul class="contact-info">
+                    <li><i class="icon-map"></i>Av. Transnordestina - UEFS, Feira de Santana-BA</li>
+                    <li><i class="icon-phone"></i>075 3161-8000</li>
+                    <li><i class="icon-envelope"></i><a href="#">contato@ecomp.uefs.br</a></li>
+                    <li><i class="icon-globe"></i><a href="#">www.yoursite.com</a></li>
+                </ul>
+            </div>
+            <div class="col-md-8 col-md-push-1 col-sm-12 col-sm-push-0 col-xs-12 col-xs-push-0">
+                <form method="post"  id="fContato">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="form-group">
+                                <select class="form-control">
+                                    <option id="">Opção 01</option>
+                                    <option id="">Opção 01</option>
+                                    <option id="">Opção 01</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <input class="form-control" placeholder="Name Completo" type="text" name="tNomeCom" id="cNomeCom" size="30" maxlength="40">
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <input class="form-control" placeholder="Email" type="email" name="tMail" id="cMail" size="30" maxlength="40" placeholder="Digite  seu e-mail">
+                            </div>
+                        </div>
+                        <div class="col-md-12">
+                            <div class="form-group">
+                                <textarea name="" class="form-control" id="" cols="30" rows="7" placeholder="Message"></textarea>
+                            </div>
+                        </div>
+                        <div class="col-md-12">
+                            <div class="form-group">
+                                <input value="Enviar Mensagem" class="btn btn-primary" type="submit">
+                            </div><!-- Fim da div Group do botão enviar -->
+                        </div><!-- Fim da div/class col-md-12 -->
+                    </div><!-- Fim da div/class row -->
+                </form><!-- Fim do form -->
+            </div><!-- Fim da div Group geral do form -->
+        </div>
+    </div>
+</div>
---
+++ view/publicar-servico-view.php
@@ -0,0 +1,58 @@
+<div class="animate-box">
+    <div class="container">
+        <div class="post">
+            <div class="col-md-8 col-md-offset-2 col-md-push-2 animate-box">
+                <div style="height: 2em"></div>
+                <h2 >Publicar Serviço</h2>
+                <p>Publique um serviço ...</p><br><br>
+
+            </div>
+            
+            <div class="col-md-10 col-md-push-1 col-sm-12 col-sm-push-0 col-xs-12 col-xs-push-0">
+                <form method="post"  id="fServico">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <select class="form-control">
+                                    <option id="">Serviço 01</option>
+                                    <option id="">Serviço 02</option>
+                                    <option id="">Serviço 03</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <input class="form-control" placeholder="Título do Serviço" type="text" name="tTituloServico" id="cTituloServico" size="30" maxlength="40">
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="form-group">
+                                <input class="form-control" placeholder="Valor (R$)" name="tValor" id="cValor" size="30" maxlength="40">
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="form-group">
+                                <input class="form-control" placeholder="Data de Iníncio" type="date" name="tDataInicio" id="cDataInicio" size="30" maxlength="40">
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="form-group">
+                                <input class="form-control" placeholder="Data Final" type="date" name="tDataFinal" id="cDataFinal" size="30" maxlength="40">
+                            </div>
+                        </div>
+                        <div class="col-md-12">
+                            <div class="form-group">
+                                <textarea name="tDescricao" class="form-control" id="cDescricao" cols="30" rows="4" placeholder="Descrição do Serviço"></textarea>
+                            </div>
+                        </div>
+                        <div class="col-md-12">
+                            <div class="form-group">
+                                <input value="Publicar Serviço" class="btn btn-primary" type="submit">
+                            </div><!-- Fim da div Group do botão enviar -->
+                        </div><!-- Fim da div/class col-md-12 -->
+                    </div><!-- Fim da div/class row -->
+                </form><!-- Fim do form -->
+            </div><!-- Fim da div Group geral do form -->
+        </div>
+    </div>
+</div>
